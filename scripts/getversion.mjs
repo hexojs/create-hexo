91023f71fdf6d720d1203168d43e3e2e6ac8a023
uiolee <22849383+uiolee@users.noreply.github.com>
2024-01-01 23:31:41 +0800

    ci: improve test, build

--- scripts/getversion.mjs
+++ scripts/getversion.mjs
@@ -1,5 +1,6 @@
 import { readFileSync } from "node:fs";
-import * as core from "@actions/core";
+import { exportVariable } from "@actions/core";
 const { version } = JSON.parse(readFileSync("package.json"));
 console.info(version);
-core.exportVariable("version", version);
+// process.stdout.write(`::set-env name=version::${version}`);
+exportVariable("version", version);
